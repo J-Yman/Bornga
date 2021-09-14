3779278943eb93f1418e8e33b02bcbd0be3fa847
gaveNothing <90594638+gaveNothing@users.noreply.github.com>
2021-09-15 00:42:07 +0900

    practice : 페이지별 라우팅
    * 정규식으로 쿼리스트링 무시, 홈페이지로 이동
    * localhost:3000/about 의 경우는 About 을 페이지에 렌더링
    * switch case 의 default 로 404 Not Found 페이지 렌더링

--- practice/helloworld.js
+++ practice/helloworld.js
@@ -1,13 +1,24 @@
 const http = require("http");
 const port = process.env.PORT || 3000;
 
-// http.createServer 메서드는 HTTP 요청 (이벤트) 이 일어날 때마다 콜백함수를 호출한다.
 const server = http.createServer((req, res) => {
-	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Hello world");
+	const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+	switch (path) {
+		case "":
+			res.writeHead(200, { "Content-type": "text/plain" });
+			res.end("Homepage");
+			break;
+		case "/about":
+			res.writeHead(200, { "Content-type": "text/plain" });
+			res.end("About");
+			break;
+		default:
+			res.writeHead(404, { "Content-type": "text/plain" });
+			res.end("Not Found");
+			break;
+	}
 });
 
-// 
 server.listen(port, () =>
 	console.log(
 		`server started on port ${port}; ` + "press Ctrl-C to teminate..."
